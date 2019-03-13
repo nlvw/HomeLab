cde4d5e2da3396bd319ab3ccfb824afbd1f5468f
Wolfereign <nvonwolff@gmail.com>
2019-03-13 17:42:44 +0000

    watch tower must run in the 'host' namespace

--- Docker/WatchTower/watchtower.sh
+++ Docker/WatchTower/watchtower.sh
@@ -5,6 +5,7 @@
 # Stand Up New container
 docker run -d  \
 	--name watchtower \
+	--userns host \
 	--restart="unless-stopped" \
 	--mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
 	v2tec/watchtower:latest --schedule="0 2 * * *" --cleanup
